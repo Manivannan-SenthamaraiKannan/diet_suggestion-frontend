4641a353ec7fc2307cd714699d5d5c010b78f371
Manivannan S <sharvamanivannan@gmail.com>
2023-06-26 10:21:40 +0000

    Food Plan Cards for recipe

--- src/Components/Receipe/FoodPlan.js
+++ src/Components/Receipe/FoodPlan.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 const FoodPlan = (props) => {
     const navigate = useNavigate();
-    const [diet, setDiet] = useState();
+    const [diet, setDiet] = useState([]);
 
     useEffect(() => {
         getDiet()
@@ -16,13 +16,9 @@ const FoodPlan = (props) => {
     const endpointAPI = 'https://diet-suggestion-backend-ixup.onrender.com'
     // axios get call
     const getDiet = () => {
-        axios.get(`${endpointAPI}/keto`).then((res) => {
-            if (res.status === 401) {
-                console.log("Data Not Found")
-            }
-            console.log(res.data)
-            setDiet(res.data)
-        })
+        axios.get(`${endpointAPI}/keto`)
+            .then((res) => setDiet(res.data))
+            .catch((err) => console.error(err))
     }
     console.log(diet)
     return (
@@ -35,15 +31,24 @@ const FoodPlan = (props) => {
                 <hr />
                 {/* cards section */}
                 <div className="card" style={{ width: '18rem' }}>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
+                    <div className="card-body" >
+                        <h5 className="card-title">Card Title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
                     </div>
                 </div>
+                {
+                    diet.forEach((element) => {
+                        // for (let i = 0; i < element.hints.length; i++) {
+                        // }
+                        element.hints.map(()=>{
+                            
+                        })
+                    })
+                }
             </div>
             <Footer />
-        </div>
+        </div >
     )
 }
 
