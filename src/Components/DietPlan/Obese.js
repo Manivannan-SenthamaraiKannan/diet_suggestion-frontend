0a086cc137d76bba3613e554d02807252c149a97
Manivannan-SenthamaraiKannan <sharvamanivannan@gmail.com>
2023-11-08 23:27:06 +0530

    Additional Diet Recipes

--- src/Components/DietPlan/Obese.js
+++ src/Components/DietPlan/Obese.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 
-const Obese = ({setDiet}) => {
+const Obese = ({ setDiet }) => {
 
     const setDietName = (diet) => {
         setDiet(diet)
@@ -11,202 +11,202 @@ const Obese = ({setDiet}) => {
 
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className="container py-3">
                 <div className='diets'>
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Keto</h5>
                             <Link to='/keto' className="btn btn-primary"
-                                  onClick={() => setDietName("keto")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                                onClick={() => setDietName("keto")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Mediterranean</h5>
-                            <Link to='/carbohydrate' className="btn btn-primary"
-                                  onClick={() => setDietName("mediterranean")}
-                                  style={{background: "#42cd8c"}}
+                            <Link to='/mediterranean' className="btn btn-primary"
+                                onClick={() => setDietName("mediterranean")}
+                                style={{ background: "#42cd8c" }}
                             >Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Intermittent-Fasting</h5>
-                            <Link to="/plant" className="btn btn-primary"
-                                  onClick={() => setDietName("fasting")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/fasting" className="btn btn-primary"
+                                onClick={() => setDietName("fasting")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Low-Carbohydrate</h5>
-                            <Link to="/vegan" className="btn btn-primary"
-                                  onClick={() => setDietName("carbohydrate")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/carbohydrate" className="btn btn-primary"
+                                onClick={() => setDietName("carbohydrate")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Plant-Based</h5>
-                            <Link to="/zone" className="btn btn-primary"
-                                  onClick={() => setDietName("plant")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/plant" className="btn btn-primary"
+                                onClick={() => setDietName("plant")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Veganism</h5>
-                            <Link to="/diabetic" className="btn btn-primary"
-                                  onClick={() => setDietName("vegan")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/vegan" className="btn btn-primary"
+                                onClick={() => setDietName("vegan")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Gluten-Free</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("gluten")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/gluten" className="btn btn-primary"
+                                onClick={() => setDietName("gluten")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Low-Fat</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("lowfat")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/lowfat" className="btn btn-primary"
+                                onClick={() => setDietName("lowfat")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Raw-Food</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("rawfood")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/rawfood" className="btn btn-primary"
+                                onClick={() => setDietName("rawfood")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Paleolithic</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("paleolithic")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/paleolithic" className="btn btn-primary"
+                                onClick={() => setDietName("paleolithic")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Weight-Watchers</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("weight")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/weight" className="btn btn-primary"
+                                onClick={() => setDietName("weight")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Very Low-Calorie</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("lowcalorie")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/lowcalorie" className="btn btn-primary"
+                                onClick={() => setDietName("lowcalorie")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">South-Beach</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("south")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/south" className="btn btn-primary"
+                                onClick={() => setDietName("south")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Atkins</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("atkins")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/atkins" className="btn btn-primary"
+                                onClick={() => setDietName("atkins")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Vegetarian</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("vegetarianism")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/vegetarianism" className="btn btn-primary"
+                                onClick={() => setDietName("vegetarianism")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Dukan</h5>
                             <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("dukan")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                                onClick={() => setDietName("dukan")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Alkaline</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("alkaline")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/alkaline" className="btn btn-primary"
+                                onClick={() => setDietName("alkaline")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Whole-30</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("whole30")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/whole30" className="btn btn-primary"
+                                onClick={() => setDietName("whole30")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Zone</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("zone")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/zone" className="btn btn-primary"
+                                onClick={() => setDietName("zone")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Diabetic</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("diabetic")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/diabetic" className="btn btn-primary"
+                                onClick={() => setDietName("diabetic")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Slim-Fast</h5>
-                            <Link to="/dukan" className="btn btn-primary"
-                                  onClick={() => setDietName("slimfast")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/slimfast" className="btn btn-primary"
+                                onClick={() => setDietName("slimfast")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
--- src/Components/DietPlan/SevereThinness.js
+++ src/Components/DietPlan/SevereThinness.js
@@ -1,57 +1,57 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 
-const SevereThinness = ({setDiet}) => {
+const SevereThinness = ({ setDiet }) => {
     const setDietName = (diet) => {
         setDiet(diet)
     }
 
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className="container py-3">
                 <div className='diets'>
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Carnivore</h5>
-                            <Link to='/keto' className="btn btn-primary"
-                                  onClick={() => setDietName("carnivore")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to='/carnivore' className="btn btn-primary"
+                                onClick={() => setDietName("carnivore")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Nutri-System</h5>
-                            <Link to='/carbohydrate' className="btn btn-primary"
-                                  onClick={() => setDietName("nutrisystem")}
-                                  style={{background: "#42cd8c"}}
+                            <Link to='/nutrisystem' className="btn btn-primary"
+                                onClick={() => setDietName("nutrisystem")}
+                                style={{ background: "#42cd8c" }}
                             >Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Alkaline</h5>
-                            <Link to="/plant" className="btn btn-primary"
-                                  onClick={() => setDietName("alkaline")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/alkaline" className="btn btn-primary"
+                                onClick={() => setDietName("alkaline")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
 
-                    <div className="card" style={{width: "18rem"}}>
+                    <div className="card" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Mediterranean</h5>
-                            <Link to="/vegan" className="btn btn-primary"
-                                  onClick={() => setDietName("mediterranean")}
-                                  style={{background: "#42cd8c"}}>Get Recipe</Link>
+                            <Link to="/mediterranean" className="btn btn-primary"
+                                onClick={() => setDietName("mediterranean")}
+                                style={{ background: "#42cd8c" }}>Get Recipe</Link>
                         </div>
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
 
     )
